1cc3ba532b6d91bf9ef5adf7d289fe6dbb6f4c59
RaphaelAbeille <118533386+RaphaelAbeille@users.noreply.github.com>
2023-11-19 10:24:08 +0100

    Update list.c

--- list.c
+++ list.c
@@ -1,27 +1,108 @@
 #include"list.h"
 #include<stdio.h>
+#include<stdlib.h>
 #include <stddef.h>
 
 
-t_list * creationListeVide(int nb_max_niveaux)
+t_list * creationListe(int nb_max_niveaux)
 {
-    t_list * l;
+    t_list * l = (t_list *)malloc(sizeof(t_list));
     l->max_level = nb_max_niveaux;
-    l->head = NULL;
+    l->head = (t_list**)malloc(nb_max_niveaux * sizeof(t_list*));
+    for(int i=0;i<nb_max_niveaux;i++)
+    {
+        l->head[i] = NULL;
+    }
     return l;
 }
-int* insererValeurTete(int val,t_list * l,t_cell * c)
+t_list * insererCellTete(t_list * l,t_cell * c,int niveau2)
 {
-    l->head = c;
+    t_cell * courant;
+    int rep2;
+    rep2 = isLevelListEmpty(*l,niveau2);
+    printf("Le niveau est il vide ? %d\n",rep2);
+    if (rep2==1)
+    {
+        l->head[niveau2] = c;
+    }
+    else
+    {
+        courant = l->head[niveau2];
+        while(courant->next[niveau2]!=NULL)
+        {
+            printf("Boucle while inserer tete\n");
+            courant = courant->next[niveau2];
+        }
+        courant->next[niveau2] = c;
+        printf("fin bcl while\n");
+    }
+
+    printf("La cellule a bien ete ajoutee A la liste \n");
+    return l;
 };
 void afficherUnNiveau(t_list l, int niveau)
 {
-    printf("[list_head_%d @-]--> %d",niveau,l.head);
+    t_cell * courant;
+    int rep = isLevelListEmpty(l,niveau);
+    printf("Le niveau est il vide ? %d\n",rep);
+    if (rep==1)
+    {
+        printf("[list head_%d]--> NULL\n",niveau);
+    }
+    else
+    {
+        courant = l.head[niveau];
+        printf("[list head_%d]-->",niveau);
+        while(courant->next[niveau]!=NULL)
+        {
+            printf("[%d|@]-->",courant->value);
+            courant = courant->next[niveau];
+        }
+        printf("[%d|@]--> NULL\n",courant->value);
+    }
 };
 void afficherTousNiveaux(t_list l)
 {
-    for(int i=0;i<l.max_level-1;i++)
+    int rep = isListEmpty(l);
+    if (rep==1)
     {
-        afficherUnNiveau(l,i);
+        for(int j=0;j<l.max_level;j++)
+        {
+            printf("[list head_%d]-->NULL\n",j);
+        }
     }
+    else
+    {
+        for(int j=0;j<l.max_level;j++)
+        {
+            afficherUnNiveau(l,j);
+        }
+
+    }
+};
+int isListEmpty(t_list l)
+{
+    int rep =1;
+    for(int i=0;i<l.max_level;i++)
+    {
+        if(l.head[i]!=NULL)
+        {
+            rep = 0;
+        }
+    }
+    return rep;
+}
+int isLevelListEmpty(t_list l, int niveau3)
+{
+    int rep;
+    if(l.head[niveau3]==NULL)
+    {
+        rep =1;
+    }
+    else
+    {
+        rep =0;
+    }
+    return rep;
+}
 };
