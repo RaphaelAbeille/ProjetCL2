3509bdf560fbf3be779a8be665809d7839155fab
RaphaelAbeille <118533386+RaphaelAbeille@users.noreply.github.com>
2023-11-19 10:24:35 +0100

    Update list.h

--- list.h
+++ list.h
@@ -6,7 +6,9 @@ typedef struct t_list1
     int max_level;
 }t_list;
 
-t_list * creationListeVide(int nb_max_niveaux);
-int* insererValeurTete(int val,t_list * l,t_cell * c);
+t_list * creationListe(int nb_max_niveaux);
+t_list * insererCellTete(t_list * l,t_cell * c,int niveau2);
 void afficherUnNiveau(t_list l,int niveau);
 void afficherTousNiveaux(t_list l);
+int isListEmpty(t_list l);
+int isLevelListEmpty(t_list l, int niveau3);
