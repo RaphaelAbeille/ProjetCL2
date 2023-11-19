bf30ad22a14fb31ffd4c40b224d3c7a34045b953
RaphaelAbeille <118533386+RaphaelAbeille@users.noreply.github.com>
2023-11-19 10:22:55 +0100

    Update cell.c

--- cell.c
+++ cell.c
@@ -1,9 +1,20 @@
 #include"cell.h"
+#include<stdio.h>
+#include<stdlib.h>
 
 t_cell * creationCell(int val,int nbniveaux)
 {
-    t_cell * t;
+    t_cell * t = (t_cell *)malloc(sizeof(t_cell));
     t->value = val;
-    t->nblevel = nbniveaux;
+    t->nb_niveau = nbniveaux;
+    t->next = (t_cell **)malloc(nbniveaux * sizeof(t_cell*));
+    for(int i=0;i<nbniveaux;i++)
+    {
+        t->next[i] = NULL;
+    }
     return t;
+}
+void afficherCell(t_cell c)
+{
+    printf("[value: %d, nb niveau : %d ]\n", c.value,c.nb_niveau);
 }
