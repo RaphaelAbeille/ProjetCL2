40e1e8b22064d01fb810edc6ea9873afee4aa802
RaphaelAbeille <118533386+RaphaelAbeille@users.noreply.github.com>
2023-12-10 11:52:29 +0100

    Update list.h

--- list.h
+++ list.h
@@ -12,3 +12,13 @@ void afficherUnNiveau(t_list l,int niveau);
 void afficherTousNiveaux(t_list l);
 int isListEmpty(t_list l);
 int isLevelListEmpty(t_list l, int niveau3);
+void insert_head(t_list * l, t_cell * c, int level);
+void print_level_list(t_list l, int level);
+void print_list(t_list l);
+void insererCellBonEndroit(t_list* liste, t_cell* cellule, int niveau);
+void print_aligned_list(t_list l);
+t_list * creationListPartie2();
+int dichotomie_research_cell(t_list * lst, t_cell * cell, int value);
+int level_0_research_list(t_list * list, int val);
+int level_0_research_cell(t_cell * cell, int val);
+
